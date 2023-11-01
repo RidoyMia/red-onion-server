d1cb3d6d2ab107d50b5dac06c0b14fce997c8ddb
RidoyMia <ridoymia153159hero@gmail.com>
2023-11-01 10:40:53 -0700

    vercel deploy for check

---
+++ src/module/Category/Category.controller.js
@@ -0,0 +1,43 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CategoryController = void 0;
+const GlobalError_1 = require("../../globalError/GlobalError");
+const Category_services_1 = require("./Category.services");
+const createCategoryController = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const data = req.body;
+        const result = yield Category_services_1.categoryService.createCategoryServices(data);
+        res.status(200).send({
+            action: true,
+            result
+        });
+    }
+    catch (error) {
+        (0, GlobalError_1.GlobalError)(error, req, res, next);
+    }
+});
+const getCategoryController = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const result = yield Category_services_1.categoryService.getCategoryServices();
+        res.status(200).send({
+            action: true,
+            result
+        });
+    }
+    catch (error) {
+        (0, GlobalError_1.GlobalError)(error, req, res, next);
+    }
+});
+exports.CategoryController = {
+    createCategoryController,
+    getCategoryController
+};
